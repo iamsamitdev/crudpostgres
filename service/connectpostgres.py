d1ec3887ced869236a80c348bfcfc7238d4eb34e
Samit Koyom <samitkoyom@gmail.com>
2023-09-21 11:56:15 +0700

    update connect

--- service/connectpostgres.py
+++ service/connectpostgres.py
@@ -2,11 +2,20 @@ import psycopg2
 
 # สร้างฟังก์ชันสำหรับเชื่อมต่อฐานข้อมูล PostgreSQL
 def connectdb():
+    # Localhost
+    # connection = psycopg2.connect(
+    #     host="localhost",
+    #     user="postgres",
+    #     password="12345678",
+    #     database="sampledb",
+    #     port=5432
+    # )
+    # Server
     connection = psycopg2.connect(
-        host="localhost",
-        user="postgres",
-        password="12345678",
-        database="sampledb",
+        host="dpg-ck5snn0s0i2c73bv7h20-a",
+        user="admin",
+        password="iSX0MfPPfZhhlzAfLqxpI9JTxTmxaMMO",
+        database="sampledb_3te8",
         port=5432
     )
     return connection
